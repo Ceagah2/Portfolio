5e7b1a0399274cd442f8a6aa83752da2fbc2cef2
Carlos Ceagah <carlosceagah@gmail.com>
2023-11-07 15:48:53 -0300

    chore: initial commit

---
+++ src/styles/theme.ts
@@ -0,0 +1,24 @@
+export const theme = {
+  colors: {
+    black: '#080708',
+    white: '#E6E8E6',
+    heading: '#129490',
+    highlight: '#CE1483',
+    highLightPurple: '#8A89C0',
+  },
+  fonts: {
+    heading: 'Poppins, sans-serif',
+    body: 'Roboto, sans-serif',
+  },
+  fontSizes: {
+    heading: '1.5rem',
+    body: '1rem',
+    small: '0.75rem',
+  },
+  fontWeights: {
+    light: 300,
+    regular: 400,
+    medium: 500,
+    bold: 700,
+  }
+}
