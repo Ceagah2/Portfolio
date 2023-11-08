f8611a3bf241d5b66ccdff79272227c66efb32da
Carlos Ceagah <carlosceagah@gmail.com>
2023-11-08 17:46:29 -0300

    feat: create an animation with sound to konami code

--- src/page/index.tsx
+++ src/page/index.tsx
@@ -1,36 +1,40 @@
 import { useEffect, useState } from "react"
 import { Header } from "../components/organisms/Header"
 import { Container } from "./styles"
+import { AnimatedImage } from "../components/atoms/AnimationImage"
+import { KonamiCode } from "../utils/konami"
+import { AudioComponent } from "../components/atoms/AudioPlay"
+
+import AudioSource from '../assets/item.wav'
 
 const Home = () => {
   const [code, setCode] = useState<boolean>(false)
+  const [isAnimationActive, setIsAnimationActive] = useState<boolean>(false)
 
+  const animationTime = () => {
+    setIsAnimationActive(true)
+    setTimeout(() => {
+      setIsAnimationActive(false)
+    }, 1000)
+  }
+
+  const startAnimation = () => {
+    animationTime()
+  };
+  
   useEffect(() => {
-    let index = 0;
-    const konamiCode = ['ArrowUp', 'ArrowUp', 'ArrowDown', 'ArrowDown', 'ArrowLeft', 'ArrowRight', 'ArrowLeft', 'ArrowRight', 'b', 'a'];
-
-    const keydownHandler = (e: KeyboardEvent) => {
-      if (e.key === konamiCode[index]) {
-        index++;
-        if (index === konamiCode.length) {
-          setCode(true)
-          index = 0;
-        }
-      } else {
-        index = 0;
-      }
-    };
-
-    window.addEventListener('keydown', keydownHandler);
-    return () => {
-      window.removeEventListener('keydown', keydownHandler);
-    };
+    KonamiCode({ setCode, startAnimation })
   }, [])
 
 
   return(
     <Container>
       <Header code={code}/>
+      {isAnimationActive && 
+      <>
+        <AudioComponent audioSrc={AudioSource} />
+        <AnimatedImage />
+      </>}
     </Container>
   )
 }
