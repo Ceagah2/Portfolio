fbced1ad8b5cf562477976c0c65351128e9b2fe9
Carlos Ceagah <carlosceagah@gmail.com>
2023-11-14 10:27:56 -0300

    refactor: refactor imports section, to better readability

--- src/page/index.tsx
+++ src/page/index.tsx
@@ -1,13 +1,14 @@
 /* eslint-disable react-hooks/exhaustive-deps */
 import { useEffect, useState } from "react"
-import { Header } from "../components/organisms/Header"
-import { Container } from "./styles"
-import { AnimatedImage } from "../components/atoms/AnimationImage"
-import { KonamiCode } from "../utils/konami"
-import { AudioComponent } from "../components/atoms/AudioPlay"
-import {messages} from '../utils/constants'
-
 import AudioSource from '../assets/item.wav'
+import { KonamiCode } from "../utils/konami"
+import {messages, BioText} from '../utils/constants'
+import { Header } from "../components/organisms/Header"
+import { AudioComponent } from "../components/atoms/AudioPlay"
+import { AnimatedImage } from "../components/atoms/AnimationImage"
+import { JobExperience } from "../components/organisms/JobExperience"
+import { SectionContainer } from "../components/atoms/SectionContainer"
+import * as S from "./styles"
 
 const Home = () => {
   const [code, setCode] = useState<boolean>(false)
@@ -38,14 +39,24 @@ const Home = () => {
 
 
   return(
-    <Container>
+    <S.Container>
       <Header code={code}/>
       {isAnimationActive && 
       <>
         <AudioComponent audioSrc={AudioSource} />
         <AnimatedImage />
       </>}
-    </Container>
+      <S.MainContainer>
+        <SectionContainer title="Bio" delay={0.5} key={1} orientation="start">
+          <S.SectionText>
+            {BioText}
+          </S.SectionText>
+        </SectionContainer>
+        <SectionContainer title="Job Experience" delay={1} key={2} orientation="end">
+          <JobExperience />
+        </SectionContainer>
+      </S.MainContainer>
+    </S.Container>
   )
 }
 
--- src/page/styles.ts
+++ src/page/styles.ts
@@ -1,13 +1,32 @@
 import styled from 'styled-components'
 import { theme } from '../styles/theme'
 
-export const Container = styled.main`
+export const Container = styled.div`
   background-color: ${theme.colors.black};
   min-height: 100vh;
   height: 100%;
+  min-width: 100vh;
+  width: 100%;
   display: flex;
   flex-direction: column;
   justify-content: flex-start;
-  align-items: flex-start;
+  align-items: center;
   padding-top: 25px;
 `; 
+
+export const MainContainer = styled.main`
+  width: 90%;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  padding: 20px;
+`
+
+export const SectionText = styled.p`
+  color: ${theme.colors.white};
+  font-size: ${theme.fontSizes.body};
+  letter-spacing: 1.1px;
+  line-height: 1.5;
+`
+
