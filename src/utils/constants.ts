954eacf2174adfc66fc26eaee491d16402d39df0
Carlos Ceagah <carlosceagah@gmail.com>
2023-11-14 10:26:13 -0300

    docs: create Jobs experiences texts

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -6,3 +6,105 @@ export const messages = [
   'You might want to try a classic gaming move to unlock something special...',
   'Think retro! What gaming code could reveal a hidden surprise?'
 ];
+
+export const BioText = `
+  My name is Carlos Henrique, and I am originally from Taubaté, a small city in the interior of the State of São Paulo. 
+  In my free time, I'm a gamer and amateur speedrunner of Resident Evil 3.
+  I've been working as a front-end developer since 2020. I decided to leave college, where I was studying Systems Analysis and Development, 
+  in the last period of 2020. This decision was influenced by my entry into the workforce, and I chose to focus on my professional career.
+  Before becoming a developer, I worked in commerce and also served as a public servant.
+  I came close to becoming a professional chef because I have a great passion for that field. 
+  I was on the verge of leaving everything at the time and enrolling in a gastronomy college. 
+  It took very little for me not to be here now, sharing my portfolio with you.
+`
+
+export const JobExperiences = [
+  {
+    id: 1,
+    title: 'Front-end Developer @ Oceaning Marketing and Training',
+    skills: [
+      {
+        js: 3,
+        css: 5,
+        html: 5,
+        react: 2,
+        native: 2,
+      }
+    ],
+    dateRange: '09/2020 to 03/2021'
+  },
+  {
+    id: 2,
+    title: 'Mobile Developer @ WT2 Blockchain and software',
+    skills: [
+      {
+        js: 3,
+        css: 5,
+        html: 5,
+        native: 4,
+      }
+    ],
+    dateRange: '03/2021 to 04/2021'
+  },
+  {
+    id: 3,
+    title: 'Mobile Developer @ LKS Health and Wellness technologies',
+    skills: [
+      {
+        js: 3,
+        css: 5,
+        html: 5,
+        native: 4,
+        ts: 3
+      }
+    ],
+    dateRange: '04/2021 to 11/2021'
+  },
+  {
+    id: 4,
+    title: 'Front-end Developer @ Tecla T software solutions',
+    skills: [
+      {
+        js: 3,
+        css: 5,
+        html: 5,
+        react: 4,
+        ts: 3,
+      }
+    ],
+    dateRange: '12/2021 to 04/2022'
+  },
+  {
+    id: 5,
+    title: 'Software Engineer @ Modern.tech (former Evolved Technologies)',
+    skills: [
+      {
+        js: 4,
+        css: 5,
+        html: 5,
+        native: 4,
+        react: 4,
+        ts: 4,
+        mongo: 2,
+      }
+    ],
+    dateRange: '04/2022 to 11/2022'
+  },
+  {
+    id: 6,
+    title: 'Front end developer @ Hero 99',
+    skills: [
+      {
+        js: 4,
+        css: 5,
+        html: 5,
+        native: 4,
+        react: 4,
+        next: 4,
+        ts: 4,
+        wordpress: 2,
+      }
+    ],
+    dateRange: '12/2022 to Present days'
+  },
+]
