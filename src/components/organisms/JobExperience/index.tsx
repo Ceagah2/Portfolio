29aa12da929dbb3cbcd766ac2be2b4cf88024c51
Carlos Ceagah <carlosceagah@gmail.com>
2023-11-20 18:15:31 -0300

    feat: finish Job Experiences Section

--- src/components/organisms/JobExperience/index.tsx
+++ src/components/organisms/JobExperience/index.tsx
@@ -1,10 +1,31 @@
 import * as S from './styles'
 import { JobExperiences } from '../../../utils/constants'
 export const JobExperience = () => {
-  console.log(JobExperiences)
-  return(
+
+  return (
     <S.Container>
-      <S.JobTitle>Job Experience</S.JobTitle>
+      {JobExperiences.map((experience) => (
+        <S.JobContainer key={experience.id}>
+          <S.JobTitle>{experience.title}</S.JobTitle>
+          <S.JobDescription>{experience.description}</S.JobDescription>
+          <S.DateContainer>
+            <S.DateText>Worked since: </S.DateText>
+            <S.DateTime>{experience.dateRange}</S.DateTime>
+          </S.DateContainer>
+          <S.SkillContainer>
+            {experience.skills.map((skill, index) => (
+              <div key={index}>
+                <S.SkillName>{Object.values(skill)[0]}</S.SkillName>
+                <S.ProgressBar>
+                  {[...Array(5)].map((_, scoreIndex) => (
+                    <S.SkillSquare score={scoreIndex < skill.score ? scoreIndex + 1 : 0} />
+                  ))}
+                </S.ProgressBar>
+              </div>
+            ))}
+          </S.SkillContainer>
+        </S.JobContainer>
+      ))}
     </S.Container>
-  )
-}
+  );
+};
--- src/components/organisms/JobExperience/styles.ts
+++ src/components/organisms/JobExperience/styles.ts
@@ -1,4 +1,14 @@
 import styled from 'styled-components'
+import { theme } from '../../../styles/theme';
+
+interface SkillSquareProps {
+  score: number;
+}
+const getColor = (score: number): string => {
+  const colors = ['#ffadad', '#ffd6a5', '#fdffb6', '#caffbf', '#9bf6ff']; 
+  return colors[score - 1] || 'black'; 
+};
+
 
 export const Container = styled.div`
   display: flex;
@@ -7,9 +17,84 @@ export const Container = styled.div`
   justify-content: center;
   width: 100%;
 `;
-export const JobTitle = styled.h1`
- color: white;
+export const JobContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: flex-end;
+  justify-content: center;
+  width: 100%;
+  margin-bottom: 20px;
 `;
-export const JobDescription = styled.p``;
-export const SkillContainer = styled.div``;
-export const SkillPoint = styled.div``;
+export const JobTitle = styled.h3`  
+ color: ${theme.colors.highLightPurple};
+ font-size: ${theme.fontSizes.mobileSubHeading};
+ font-family: ${theme.fonts.heading};
+`;
+export const JobDescription = styled.p`
+  color: ${theme.colors.white};
+  font-size: ${theme.fontSizes.body};
+  font-family: ${theme.fonts.body};
+  text-align: right;
+`;
+export const DateContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: flex-end;
+  width: 100%;
+  margi-top: 10px;
+`;
+export const DateText = styled.span`
+  text-align: right;
+  color: ${theme.colors.highlight};
+  font-size: ${theme.fontSizes.body};
+  font-family: ${theme.fonts.heading};
+  margin-right: 5px;
+`;
+export const DateTime = styled.span`
+   text-align: right;
+   color: ${theme.colors.white};
+   font-size: ${theme.fontSizes.body};
+`;
+export const SkillContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: flex-end;
+  justify-content: center;
+  width: 100%;
+`;
+export const SkillWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: flex-end;
+  justify-content: center;
+  width: 50%;
+`;
+export const SkillName = styled.span`
+  color: ${theme.colors.white};
+  font-size: ${theme.fontSizes.body};
+  font-family: ${theme.fonts.body};
+
+`;
+
+export const ProgressBar = styled.div`
+  display: flex;
+  justify-content: flex-start;
+  align-items: center;
+  height: 20px;
+  margin-top: 5px;
+`;
+
+export const SkillSquare = styled.div<SkillSquareProps>`
+  width: 20px;
+  height: 20px;
+  border-radius: 4px;
+  margin-right: 4px;
+  background-color: ${({ score }) => getColor(score)};
+  border: 1px solid white;
+`;
+
+export const Progress = styled.div`
+  height: 100%;
+  border-radius: 5px;
+`;
