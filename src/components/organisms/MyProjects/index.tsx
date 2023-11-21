6423249929019764be8863f638b8ba57c81b17bb
Carlos Ceagah <carlosceagah@gmail.com>
2023-11-21 14:04:02 -0300

    feat: finish my projects section

--- src/components/organisms/MyProjects/index.tsx
+++ src/components/organisms/MyProjects/index.tsx
@@ -1,10 +1,53 @@
-import { PersonalProjects } from "../../../utils/constants"
+import {useState, useEffect} from 'react'
+import { IProjectCard } from '../../../@types/types'
+import { USER_NAME } from '../../../utils/constants'
+import { ProjectCard } from '../../../components/atoms/ProjectCard'
 import * as S from './styles'
 export const MyProjects = () => {
-  console.log(PersonalProjects)
+  const [projects, setProjects] = useState<IProjectCard[]>()
+
+  
+  useEffect(() => {
+    const fetchProjects = () => {
+      fetch(`https://api.github.com/users/${USER_NAME}/repos`)
+        .then(response => {
+          if (!response.ok) {
+            throw new Error(`Erro ao buscar repositórios: ${response.status}`);
+          }
+          return response.json();
+        })
+        .then(data => {
+          const filteredRepos = data.filter((repo: any) => {
+            return repo.name === 'Portfolio' || repo.name === 'wise-task';
+          });
+
+          const formattedData: IProjectCard[] = filteredRepos.map((repo: any) => ({
+            id: repo.id,
+            title: repo.name,
+            description: repo.description || '',
+            owner: {
+              avatar_url: repo.owner.avatar_url,
+            },
+            html_url: repo.html_url,
+          }));
+
+          setProjects(formattedData);
+        })
+        .catch(error => {
+          console.error('Erro:', error);
+        });
+    };
+    fetchProjects()
+  },[])
+
   return(
     <S.Container>
-      <S.ProjectTitle>PROJECTS</S.ProjectTitle>
+      <S.ProjectsHeader>
+        <S.ProjectText>Below you'll find my most relevant projects, created for personal purposes. In this section, you won't find any project I've worked on, but rather the projects I've developed to launch and host on stores.</S.ProjectText>
+      </S.ProjectsHeader>
+      {projects && projects.map((project) => (
+        <ProjectCard key={project.id} {...project} />
+      ))}
     </S.Container>
   )
 }
--- src/components/organisms/MyProjects/styles.ts
+++ src/components/organisms/MyProjects/styles.ts
@@ -1,5 +1,28 @@
 import styled from 'styled-components'
+import {theme} from '../../../styles/theme'
 
-export const Container = styled.div``;
-export const ProjectTitle = styled.h3``;
-export const ProjectDescription = styled.p``;
+export const Container = styled.div`
+  width: 50vw;
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+  align-items: flex-start;
+  justify-content: flex-start;
+`;
+
+export const ProjectsHeader = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  justify-content: flex-start;
+`
+export const ProjectText = styled.p`
+  width: 60%;
+  color: ${theme.colors.white};
+  font-family: ${theme.fonts.body};
+  font-size: ${theme.fontSizes.body};
+  font-weight: bold;
+  text-align: start;
+  margin-bottom: 20px;
+`
