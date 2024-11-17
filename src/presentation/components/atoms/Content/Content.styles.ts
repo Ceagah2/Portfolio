667d0648b6bd15ddf2385194af9d7cb84d0aa090
Ceagah <carlosceagah@gmail.com>
2024-11-17 11:11:23 -0300

    style: refactor content style

--- src/presentation/components/atoms/Content/Content.styles.ts
+++ src/presentation/components/atoms/Content/Content.styles.ts
@@ -8,8 +8,7 @@ export const Container = styled.section`
   display: flex;
   flex-direction: column;
   justify-content: flex-start;
-  align-items: center;
+  align-items: flex-start;
   background-image: linear-gradient(${theme.colors.headerGradientFrom}, ${theme.colors.headerGradientTo});
-
-
+  padding: 16px;
 `
