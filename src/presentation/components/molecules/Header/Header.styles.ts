895a1cb2e255141c8440d09ba3e93b3963e429c7
Ceagah <carlosceagah@gmail.com>
2024-11-17 11:11:46 -0300

    feat: implement konami code easter egg

--- src/presentation/components/molecules/Header/Header.styles.ts
+++ src/presentation/components/molecules/Header/Header.styles.ts
@@ -2,18 +2,20 @@ import styled from 'styled-components';
 import { colors, fonts } from '../../../theme';
 
 export const Container = styled.header`
-  width: 100%;
-  height: 20vh;
+  width: 50%;
+  height: 25vh;
   display: flex;
   flex-direction: row;
   justify-content: space-between;
   align-items: center;
   padding: 1rem;
-`
+  background: ${colors.headerGradientFrom};
+
+`;
 
 export const ProfileContainer = styled.div`
-  width: 200px;
-  height: 200px;
+  width: 250px;
+  height: 250px;
   position: relative;
   display: flex;
   justify-content: center;
@@ -31,22 +33,27 @@ export const Frame = styled.img`
 `;
 
 export const ProfileImage = styled.img`
-  width: 160px; 
-  height: 160px;
+  width: 200px; 
+  height: 200px;
   border-radius: 50%;
   object-fit: cover;
 `;
 
 export const DataContainer = styled.div`
-  width: 50%;
+  width: 45%;
   height: 100%;
-  border: 1px solid white;
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  justify-content: center;
+  padding-left: 1rem;
+
 `
 
 export const NickName = styled.h2`
   color: ${colors.textPrimary};
   font-family: ${fonts.primary};
-  font-size: ${fonts.sizes.medium};
+  font-size: ${fonts.sizes.large};
 `
 
 export const FullName = styled.span`
@@ -56,13 +63,61 @@ export const FullName = styled.span`
 `
 
 export const Bio = styled.p`
-  color: ${colors.textSecondary};
+  color: ${colors.textPrimary};
   font-family: ${fonts.primary};
   font-size: ${fonts.sizes.small};
+  margin-top: 1rem;
 `;
 
 export const ExperienceContainer = styled.div`
-  width: 30%;
+  width: 25%;
   height: 100%;
-  border: 1px solid white;
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  justify-content: flex-start;
 `;
+export const TitleLine = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  border-bottom: 1px solid ${colors.border};
+  padding-bottom: 10px;
+  margin-bottom: 1rem;
+`
+export const ExperienceTitle = styled.span`
+  color: ${colors.textPrimary};
+  font-family: ${fonts.primary};
+  font-size: ${fonts.sizes.large};
+`
+export const ExperienceText = styled.span`
+  color: ${colors.accent};
+  font-family: ${fonts.primary};
+  font-size: ${fonts.sizes.medium};
+  border: 3px solid ${colors.accent};
+  padding: 0.2rem;
+  border-radius: 50%;
+`;
+
+export const StackContainer = styled.div`
+  height: 100%;
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: space-between;
+  border-radius: 8px;
+  padding-top: 8px;
+  background: ${colors.backgroundLight}
+`
+export const StackTitle = styled.h3`
+  color: ${colors.textPrimary};
+  font-family: ${fonts.primary};
+  font-size: ${fonts.sizes.large};
+`;
+export const StackText = styled.span`
+  color: ${colors.textSecondary};
+  font-family: ${fonts.primary};
+  font-size: ${fonts.sizes.small};
+`
--- src/presentation/components/molecules/Header/Header.tsx
+++ src/presentation/components/molecules/Header/Header.tsx
@@ -1,19 +1,96 @@
-import { AVATAR_URL } from "../../../../infra/utils/constants";
+import { useAtom } from 'jotai';
+import { useEffect, useState } from 'react';
+import { useTranslation } from "react-i18next";
+import { secretCodeAtom } from "../../../../data/store/Atoms";
+import messages from '../../../../infra/translate/messages.json';
+import { AVATAR_URL, techStack } from "../../../../infra/utils/constants";
+import { KonamiCode } from '../../../../infra/utils/konami';
 import frameGif from '../../../assets/frame-gif.png';
+import AudioSource from '../../../assets/item.wav';
+import { AnimatedImage, AudioComponent } from '../../atoms';
 import * as S from './Header.styles';
+
+
 export const Header = () => {
+  const [yearsOfExperience, setYearsOfExperience] = useState<number>(0)
+  const [isAnimationActive, setIsAnimationActive] = useState<boolean>(false);
+  const [secretCode, setSecretCode] = useAtom(secretCodeAtom);
+  const { t } = useTranslation();
+
+  const animationTime = () => {
+    setIsAnimationActive(true);
+    setTimeout(() => {
+      setIsAnimationActive(false);
+    }, 1000);
+  };
+
+  useEffect(() => {
+    const startAnimation = () => {
+      animationTime();
+    };
+
+    KonamiCode({ setSecretCode, startAnimation });
+  }, [setSecretCode]);
+
+  useEffect(() => {
+    const getYearsOfExperience = () => {
+      const startDate = new Date(2020, 7, 1); 
+      const today = new Date();
+      const diffInMilliseconds = today.getTime() - startDate.getTime();
+      const yearsDiff = diffInMilliseconds / (1000 * 60 * 60 * 24 * 365.25); 
+      setYearsOfExperience(Math.ceil(yearsDiff));
+    };
+
+    function showRandomMessage() {
+      const randomMessage = (messages as { messages: string[] }).messages[
+        Math.floor(
+          Math.random() * (messages as { messages: string[] }).messages.length
+        )
+      ];
+
+      console.log("🎮 Gaming Easter Egg: ", randomMessage);
+    }
+    showRandomMessage();
+
+    getYearsOfExperience()
+  },[])
   return (
     <S.Container>
+      {isAnimationActive && (
+        <>
+          <AudioComponent audioSrc={AudioSource} />
+          <AnimatedImage />
+        </>
+      )}
       <S.ProfileContainer>
         <S.Frame src={frameGif} alt="Frame de Fogo" />
         <S.ProfileImage src={AVATAR_URL} alt="Imagem de Perfil" />
       </S.ProfileContainer>
       <S.DataContainer>
-
+        <S.NickName>Carlos Ceagah</S.NickName>
+        <S.FullName>Carlos Henrique de Moura - 🇧🇷 São Paulo </S.FullName>
+        <S.Bio>{t("bio")}</S.Bio>
       </S.DataContainer>
 
       <S.ExperienceContainer>
-        
+        <S.TitleLine>
+          <S.ExperienceTitle>{t("terms.experience")}</S.ExperienceTitle>
+          <S.ExperienceText>{yearsOfExperience}</S.ExperienceText>
+        </S.TitleLine>
+        {secretCode && (
+          <S.TitleLine>
+            <S.ExperienceTitle>{t("terms.experience")}</S.ExperienceTitle>
+            <S.ExperienceText>{yearsOfExperience}</S.ExperienceText>
+          </S.TitleLine>
+        )}
+        <S.StackContainer>
+          <S.StackTitle>Stack</S.StackTitle>
+          {techStack.map((tech, index) => (
+            <S.StackText key={index}>
+              {tech.name} {tech.emoji}
+            </S.StackText>
+          ))}
+        </S.StackContainer>
       </S.ExperienceContainer>
     </S.Container>
   );
