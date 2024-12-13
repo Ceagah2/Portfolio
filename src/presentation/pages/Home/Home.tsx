1ed2beeb7d1756bb980ff02d8bb02d0761d91e56
Ceagah <carlosceagah@gmail.com>
2024-12-13 15:01:12 -0300

    feat: implement sidebar on home screen

--- src/presentation/pages/Home/Home.tsx
+++ src/presentation/pages/Home/Home.tsx
@@ -1,5 +1,6 @@
+
 import { useTranslation } from "react-i18next";
-import { Container, Content, Section } from "../../components/atoms";
+import { Container, Content, Section, Sidebar } from "../../components/atoms";
 import { Header } from "../../components/molecules";
 
 export default function Home() {
@@ -7,9 +8,18 @@ export default function Home() {
 
   return (
     <Container>
-        <Header/>
+      <Header />
       <Content>
-       <Section title={(t('terms.job'))} redirectLink="/"> </Section>
+        <Section title={t("terms.job")} redirectLink="/">
+          {" "}
+        </Section>
+        <Section title={t("terms.projects")} redirectLink="/">
+          {" "}
+        </Section>
+        <Section title={t("terms.certificates")} height="15vh">
+          {" "}
+        </Section>
+        <Sidebar />
       </Content>
     </Container>
   );
