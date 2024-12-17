d2f06e93f5c92dc2baf2e8b5ac8d9860ec209fce
Ceagah <carlosceagah@gmail.com>
2024-12-17 13:39:01 -0300

    refactor: rebrand on stack file

--- src/infra/utils/constants.ts
+++ src/infra/utils/constants.ts
@@ -5,15 +5,59 @@ export const IMAGE_BG_URL =
 export const USER_NAME = 'Ceagah2'
 export const AVATAR_URL = "https://github.com/Ceagah2.png"
 export const techStack = [
-  { name: 'Javascript', emoji: '💻' },
-  { name: 'React', emoji: '⚛️' },
-  { name: 'React Native', emoji: '📱' },
-  { name: 'Typescript', emoji: '🔵' },
-  { name: 'Node.js', emoji: '🟩' },
-  { name: 'MongoDB', emoji: '🍃' },
-  { name: 'Nest.js', emoji: '🐦' },
-  { name: 'Vue.js', emoji: '🌐' },
-  { name: 'Next.js', emoji: '⚡' },
-  { name: 'Expo', emoji: '📦' },
-  { name: 'Figma', emoji: '🎨' }
-]
+  {
+    name: "Javascript",
+    badgeUrl:
+      "https://img.shields.io/badge/-Javascript-F7DF1E?logo=javascript&logoColor=white&style=for-the-badge",
+  },
+  {
+    name: "React",
+    badgeUrl:
+      "https://img.shields.io/badge/-React-61DAFB?logo=react&logoColor=white&style=for-the-badge",
+  },
+  {
+    name: "React Native",
+    badgeUrl:
+      "https://img.shields.io/badge/-React%20Native-61DAFB?logo=react&logoColor=white&style=for-the-badge",
+  },
+  {
+    name: "Typescript",
+    badgeUrl:
+      "https://img.shields.io/badge/-TypeScript-007ACC?logo=typescript&logoColor=white&style=for-the-badge",
+  },
+  {
+    name: "Node.js",
+    badgeUrl:
+      "https://img.shields.io/badge/-Node.js-339933?logo=nodedotjs&logoColor=white&style=for-the-badge",
+  },
+  {
+    name: "MongoDB",
+    badgeUrl:
+      "https://img.shields.io/badge/-MongoDB-47A248?logo=mongodb&logoColor=white&style=for-the-badge",
+  },
+  {
+    name: "Nest.js",
+    badgeUrl:
+      "https://img.shields.io/badge/-NestJS-E0234E?logo=nestjs&logoColor=white&style=for-the-badge",
+  },
+  {
+    name: "Vue.js",
+    badgeUrl:
+      "https://img.shields.io/badge/-Vue.js-4FC08D?logo=vuedotjs&logoColor=white&style=for-the-badge",
+  },
+  {
+    name: "Next.js",
+    badgeUrl:
+      "https://img.shields.io/badge/-Next.js-000000?logo=nextdotjs&logoColor=white&style=for-the-badge",
+  },
+  {
+    name: "Expo",
+    badgeUrl:
+      "https://img.shields.io/badge/-Expo-000020?logo=expo&logoColor=white&style=for-the-badge",
+  },
+  {
+    name: "Figma",
+    badgeUrl:
+      "https://img.shields.io/badge/-Figma-F24E1E?logo=figma&logoColor=white&style=for-the-badge",
+  },
+];
