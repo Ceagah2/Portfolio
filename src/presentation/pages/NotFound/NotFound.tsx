f976d4635fe2d758fb8221e1cdd14dc44a5860cd
Ceagah <carlosceagah@gmail.com>
2024-12-20 12:22:51 -0300

    feat: create 404 screen

--- src/presentation/pages/NotFound/NotFound.tsx
+++ src/presentation/pages/NotFound/NotFound.tsx
@@ -1,9 +1,16 @@
-import { Container } from "../../components/atoms";
+
+import * as S from './NotFound.styles';
 
 export default function NotFound() {
+  const handleBackClick = () => {
+    window.location.href = "/";
+  };
+
   return (
-    <Container>
-      <h1>404 - Not Found</h1>
-    </Container>
+    <S.Container>
+      <S.Title>404</S.Title>
+      <S.Subtitle>Oops! A página que você está procurando não existe.</S.Subtitle>
+      <S.BackButton onClick={handleBackClick}>Voltar para a página inicial</S.BackButton>
+    </S.Container>
   );
 }
