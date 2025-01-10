5ef5fdb28c6170a9fc9618b9334683d8e3a88bfc
Ceagah <carlosceagah@gmail.com>
2025-01-10 16:21:09 -0300

    feat: update fun facts badges

--- src/presentation/components/molecules/Badge/Badge.tsx
+++ src/presentation/components/molecules/Badge/Badge.tsx
@@ -1,21 +1,37 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import engFacts from "../../../../data/github/funfacts.eng.json";
 import ptFacts from "../../../../data/github/funfacts.pt.json";
 import { BadgesProps, FunFact } from "./Badge.interface";
 import * as S from "./Badge.styles";
 
-
-
 export const Badges: React.FC<BadgesProps> = ({ language }) => {
+  const [images, setImages] = useState<Record<string, string>>({});
   const badgesData: FunFact[] =
     language === "en" ? engFacts.funFacts : ptFacts.funFacts;
+
+  useEffect(() => {
+    const loadImages = async () => {
+      const importedImages: Record<string, string> = {};
+      for (const fact of badgesData) {
+        importedImages[fact.icon] = (
+          await import(`../../../assets/funFactIcons/${fact.icon}`)
+        ).default;
+      }
+      setImages(importedImages);
+    };
+
+    loadImages();
+  }, [badgesData, language]); 
 
   return (
     <S.BadgeWrapper>
       <S.BadgeContainer>
         {badgesData.map((badge) => (
           <div key={badge.id} className="badge">
-            <S.BadgeIcon src={badge.icon} alt={badge.name} />
+            <S.BadgeIcon
+              src={images[badge.icon]} 
+              alt={badge.name}
+            />
             <S.BadgeTooltip>
               <span>{badge.name}</span>
               <p>{badge.description}</p>
