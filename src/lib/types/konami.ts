2610a7fcdbe0bbb43ae81e535e30fc1d11570112
Ceagah <carlosceagah@gmail.com>
2025-08-01 14:06:43 -0300

    feat: add konami code

---
+++ src/lib/types/konami.ts
@@ -0,0 +1,4 @@
+export interface IKonami {
+  setSecretCode: (code: boolean) => void;
+  startAnimation: () => void;
+}
---
+++ src/utils/konami.ts
@@ -0,0 +1,34 @@
+import { IKonami } from "@/lib/types/konami";
+
+export const KonamiCode = ({ setSecretCode, startAnimation }: IKonami) => {
+  let index = 0;
+  const konamiCode = [
+    "ArrowUp",
+    "ArrowUp",
+    "ArrowDown",
+    "ArrowDown",
+    "ArrowLeft",
+    "ArrowRight",
+    "ArrowLeft",
+    "ArrowRight",
+    "b",
+    "a",
+  ];
+  const keydownHandler = (e: KeyboardEvent) => {
+    if (e.key === konamiCode[index]) {
+      index++;
+      if (index === konamiCode.length) {
+        setSecretCode(true);
+        startAnimation();
+        index = 0;
+      }
+    } else {
+      index = 0;
+    }
+  };
+
+  window.addEventListener("keydown", keydownHandler);
+  return () => {
+    window.removeEventListener("keydown", keydownHandler);
+  };
+};
